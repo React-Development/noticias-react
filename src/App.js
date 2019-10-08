a6b628f12ceb630abd2f9eac12dc68bbb6118f70
bgarcia95 <bgarcia.95@outlook.com>
2019-10-08 00:34:16 -0600

    Recibiendo data de api news y agregando los articulos a nuestro state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
 class App extends Component {
-  state = {  }
-  render() { 
-    return (<h1>Noticias API React</h1> );
+  state = {
+    noticias: []
+  };
+
+  componentDidMount() {
+    this.consultarNoticias();
+  }
+
+  consultarNoticias = async () => {
+    const url = ` https:newsapi.org/v2/top-headlines?country=us&category=business&apiKey=d13f7fded7ca4c3b9fb831a3b22b60af`;
+
+    const respuesta = await axios.get(url);
+
+    const { articles: noticias } = await respuesta.data;
+
+    this.setState({ noticias });
+
+    console.log("Noticias", this.state);
+  };
+
+  render() {
+    return <h1>Noticias API React</h1>;
   }
 }
- 
+
 export default App;
